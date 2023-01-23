52c755007af3f58d922b07acc5f337b6d96210a5
Maxim Laptev <justsaynya@gmail.com>
2023-01-23 19:14:51 +0300

    regex to NDFA

---
+++ all_in_one/all_in_one.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+from .lab2 import *
+from .lab3_2 import *
+from .lab4 import *
+
+if __name__ == "__main__":
+    expected_arg_count = 3
+    if len(sys.argv) != expected_arg_count:
+        print('usage: python lab4.py "input_file" "output_file"')
+        print("example: python lab3_2.py regex.txt output.csv")
+        sys.exit()
+
+    with open(sys.argv[1], 'r') as file:
+        regex = file.read()
+
+    automata = regex_to_NDFA(self.lexemes[i]['regex'])
+    automata = NDFA_to_DFA(automata, print_state_labels=False)
+    automata = moore_minimize(automata)
+
+
+    np.savetxt(sys.argv[2], automata, delimiter=';', fmt='%s')
